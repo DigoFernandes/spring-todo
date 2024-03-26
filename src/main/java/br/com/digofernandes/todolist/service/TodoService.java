e67d2c4e2d01b7cd217d993d180bce63a3b1b000
DigoFernandes <digofernandes017@gmail.com>
2024-03-26 18:55:48 -0300

    controller configurado

--- src/main/java/br/com/digofernandes/todolist/service/TodoService.java
+++ src/main/java/br/com/digofernandes/todolist/service/TodoService.java
@@ -2,6 +2,7 @@ package br.com.digofernandes.todolist.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.digofernandes.todolist.entity.Todo;
@@ -10,26 +11,32 @@ import br.com.digofernandes.todolist.repository.TodoRepository;
 @Service
 public class TodoService {
     private TodoRepository todoRepository;
-    
+
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
-    public List<Todo> create() {
-
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
     }
 
     public List<Todo> list() {
 
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending());
+
+        return todoRepository.findAll(sort);
     }
 
-    public List<Todo> update() {
-
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
     }
 
-    public List<Todo> delete() {
-
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
     }
 
-   
 }

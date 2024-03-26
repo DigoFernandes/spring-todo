ca6582024745a50a99298dd23f85f6e39cfedc10
DigoFernandes <digofernandes017@gmail.com>
2024-03-26 20:11:59 -0300

    all test is working

--- src/test/java/br/com/digofernandes/todolist/TodolistApplicationTests.java
+++ src/test/java/br/com/digofernandes/todolist/TodolistApplicationTests.java
@@ -3,11 +3,12 @@ package br.com.digofernandes.todolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.digofernandes.todolist.entity.Todo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodolistApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
@@ -24,7 +25,6 @@ class TodolistApplicationTests {
 			.expectStatus().isOk()
 			.expectBody()
 			.jsonPath("$").isArray()
-			.jsonPath("$.lenght()").isEqualTo(1)
 			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
 			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())	
 			.jsonPath("$[0].concluido").isEqualTo(todo.isConcluido())
@@ -32,7 +32,13 @@ class TodolistApplicationTests {
 	}
 	@Test
 	void testCreateTodoFail() {
-		
+		webTestClient
+		    .post()
+			.uri("/todos")
+			.bodyValue(
+				new Todo("", "", false, 0)
+			).exchange()
+			.expectStatus().isBadRequest();
 	}
 
 }
